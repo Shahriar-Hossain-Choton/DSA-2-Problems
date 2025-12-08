fd67591fa0ccb1d26a322e4eb18e9275e3258154
Shahriar Hossain Choton <140255211+Shahriar-Hossain-Choton@users.noreply.github.com>
2025-12-08 23:42:15 +0600

    Organizing codes and printing them
    Organizing codes and sorting them in a descending order finally printing the first 3.

--- Practice.cpp
+++ Practice.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-    int n;
-    vector<string> name;
-    cout << "Whats the size?"<<endl;
-    cin >> n;
-    cout << "What are the elements?:"<<endl;
-    for(int i= 0;i<n;i++){
-        string a;
-        cin >> a;
-        name.push_back(a);
-    }
-    sort(name.begin(),name.end());
-    set<string> s(name.begin(),name.end());
-    name.assign(s.begin(),s.end());
-    for(int i= 0;i<name.size();i++){
-        cout<<name[i]<<endl;
-    }
+  vector<int> codes;
+  int n;
+  cout << "Number of codes:" << endl;
+  cin >> n;
+  cout << "What are the Codes?"<<endl;
+  int temp;
+  for(int i = 0; i<n;i++){
+    cin >> temp;
+    codes.push_back(temp);
+  }
+  sort(codes.begin(),codes.end(),greater<int>());
+  for(int i = 0; i<3;i++){
+    cout<<codes[i]<<" ";
+  }
 }
